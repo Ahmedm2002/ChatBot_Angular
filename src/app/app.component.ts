bb7c14ca7bd170e86fc90af9a516a85816e26a8b
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-07-15 14:48:10 +0500

    added tailwind & created layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Components/header/header.component';
+import { ChatComponent } from './Components/chat/chat.component';
+import { UserInputComponent } from './Components/user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ChatComponent, UserInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ChatBot_Angular';
