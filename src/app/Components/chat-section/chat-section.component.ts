e2500945f4690b841cbcc812ec9f7f22f1ebcc94
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-07-16 14:18:02 +0500

    designed user-input field

--- src/app/Components/chat-section/chat-section.component.ts
+++ src/app/Components/chat-section/chat-section.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { UserInputComponent } from '../user-input/user-input.component';
 
 @Component({
   selector: 'app-chat-section',
-  imports: [],
+  imports: [UserInputComponent],
   templateUrl: './chat-section.component.html',
-  styleUrl: './chat-section.component.css'
+  styleUrl: './chat-section.component.css',
 })
-export class ChatSectionComponent {
-
-}
+export class ChatSectionComponent {}
