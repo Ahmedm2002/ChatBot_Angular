3f31e5bc01178c1a3565d6b754a906a16f638730
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-07-20 21:28:23 +0500

    removed unnecessary componnet

--- src/app/Components/chat-section/chat-section.component.ts
+++ src/app/Components/chat-section/chat-section.component.ts
@@ -1,25 +1,32 @@
 import { Component, ElementRef, inject, ViewChild } from '@angular/core';
-import { UserInputComponent } from '../user-input/user-input.component';
 import { CommonModule } from '@angular/common';
 import { ChatService } from '../../Core/Services/chat.service';
 import { MarkdownModule } from 'ngx-markdown';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-section',
-  imports: [UserInputComponent, CommonModule, MarkdownModule],
+  imports: [CommonModule, MarkdownModule, FormsModule],
   templateUrl: './chat-section.component.html',
   styleUrl: './chat-section.component.css',
 })
 export class ChatSectionComponent {
   chatServ = inject(ChatService);
-  disableUserInput: boolean = false;
+  isInputDisabled: boolean = false;
 
   @ViewChild('chatSection') chatContainer!: ElementRef;
 
   chat: any = [];
-  receiveMessage(message: string) {
-    this.chat.push({ user: message });
-    this.disableUserInput = true;
+
+  userMessage: string = '';
+
+  sendPrompt() {
+    if (this.isInputDisabled || this.userMessage.length === 0) return;
+
+    this.chat.push({ user: this.userMessage });
+    this.isInputDisabled = true;
+
+    this.userMessage = '';
 
     setTimeout(() => {
       const container = this.chatContainer.nativeElement;
@@ -27,7 +34,7 @@ export class ChatSectionComponent {
       container.scrollTop = container.scrollHeight;
     });
 
-    this.chatServ.userMessage(message).subscribe((res: any) => {
+    this.chatServ.userMessage(this.userMessage).subscribe((res: any) => {
       this.chat[this.chat.length - 1] = {
         ...this.chat[this.chat.length - 1],
         bot: res.botReply,
@@ -37,7 +44,7 @@ export class ChatSectionComponent {
 
         container.scrollTop = container.scrollHeight;
       });
-      this.disableUserInput = false;
+      this.isInputDisabled = false;
     });
   }
 }
